904d1dd01ebbad34639c8ae935eb09f122391736
Rastko Mitrovic <rastko.mitrovic@bosch.com>
2022-02-02 18:03:37 +0100

    First commit

---
+++ src/main/java/com/fon/bg/ac/rs/cvbuilder/CvBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.fon.bg.ac.rs.cvbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvBuilderApplication.class, args);
+	}
+
+}
